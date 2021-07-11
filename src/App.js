f7bb8fd1b614939a3e6a26d020d602b051c96976
andre botta <alcbotta@gmail.com>
2021-07-11 06:52:09 +0000

    first version

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { Form, FormGroup, Input, Label, Button } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h3 className="pb-3">
+            Por favor, preencha o formulário abaixo para demonstrar o seu
+            interesse em aprender a programar.
+          </h3>
+        <div className="blurred-box">
+          <div className="student-form">
+            <Form
+              action="https://formspree.io/andre.emef.montecarlo@gmail.com"
+              method="POST"
+              name="sentMessage"
+              id="contactForm"
+              noValidate={false}
+            >
+              <FormGroup>
+                <Label>Nome</Label>
+                <Input
+                  required={true}
+                  type="text"
+                  name="contact-name"
+                  id="contact-name"
+                  placeholder={"Seu nome"}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label>{"Email"}</Label>
+                <Input
+                  required
+                  type="email"
+                  name="contact-email"
+                  id="contact-email"
+                  placeholder={"Seu email"}
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label>{"Você tem acesso à um computador?"}</Label>
+                <Input
+                  required
+                  type="textarea"
+                  name="contact-personal-computer"
+                  id="contact-personal-computer"
+                  placeholder={"Sim/Não ou explique"}
+                />
+              </FormGroup>
+              <Button color="primary" className="send-button">
+                Enviar
+              </Button>
+            </Form>
+          </div>
+        </div>
       </header>
     </div>
   );
